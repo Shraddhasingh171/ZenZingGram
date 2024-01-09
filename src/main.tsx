fd11b57e8c336d2280c09427e995fc2d2f0c7c47
Shraddha Singh <91796853+Shraddhasingh171@users.noreply.github.com>
2024-01-10 01:05:51 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import ReactDom from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDom.createRoot(document.getElementById('root')!).render(
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+   
 )
